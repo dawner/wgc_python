8e7d3de27adc687a2b9412d6450b0401e6c978fc
Dawn Richardson <dawn.richardson@abletech.co.nz>
2015-03-05 11:31:14 +1300

    Replace word list with food list; minor tweaks to output

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@
 
 import random
 
-words_file = open("words.txt", 'r').read() # Open a file for reading
+words_file = open("words.txt", 'r').read() # Open a file and read it
 word_array = words_file.split("\n") # Break it up into a list
 computer_word = random.choice(word_array) # Pick a random word from the list
 
@@ -12,9 +12,9 @@ guesses_left = 8
 while True: #Loop forever
   for letter in computer_word:
     if letter in past_guesses:
-      print(letter), #NB: trailing comma avoids printing a newline character, \n
+      print letter, #NB: trailing comma avoids printing a newline character, \n
     else:
-      print("_"),
+      print "_",
 
   print "\n\nYou have", guesses_left, "guesses left."
   guest_letter = raw_input("Guess a letter: ") # Ask user to input a letter
@@ -28,11 +28,13 @@ while True: #Loop forever
       if letter not in past_guesses: solved_word =  False #Havn't guessed the full word yet
 
     if solved_word:
-      print "Congratulations, you guessed the word!"
+      print "\n", computer_word
+      print "Congratulations, you guessed the word! \(^.^)/"
       break
   else:
     guesses_left = guesses_left - 1
 
     if guesses_left == 0:
-      print "Sorry, but you've run out of guesses. The word was %s." % computer_word
+      print "Sorry, but you've run out of guesses. (~_~) "
+      print "The word was %s." % computer_word
       break
