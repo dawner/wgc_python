3a1774fbca5b3d0e7722c0b90269e2216a1a08c8
Dawn Richardson <dawn.richardson@abletech.co.nz>
2015-03-06 10:10:38 +1300

    Add extension exercises

--- hangman.py
+++ hangman.py
@@ -2,8 +2,10 @@
 
 import random
 
-words_file = open("words.txt", 'r').read() # Open a file and read it
-word_array = words_file.split("\n") # Break it up into a list
+words_file = open("words.txt", 'r') # Open a file
+file_contents = words_file.read() # Read the file
+word_array = file_contents.split("\n") # Break it up into a list
+
 computer_word = random.choice(word_array) # Pick a random word from the list
 
 past_guesses = []
@@ -12,29 +14,40 @@ guesses_left = 8
 while True: #Loop forever
   for letter in computer_word:
     if letter in past_guesses:
-      print(letter, end=" ") # 'end=" "' puts a space instead of a newline character, \n
+      print(letter, end=" ") # 'end=" "' puts a space at the end instead of a newline character, \n
     else:
       print("_", end=" ")
 
   print("\n\nYou have", guesses_left, "guesses left.")
-  guest_letter = input("Guess a letter: ") # Ask user to input a letter
 
-  past_guesses.append(guest_letter)
+  player_guess = input("Guess a letter: ") # Ask user to input a letter
+  while len(player_guess) != 1 or not player_guess.isalpha():
+    # Print error if input is not a single character or a non-alphabetic character
+    print("Guess must be a single letter! Try again.\n")
+    player_guess = input("Guess a letter: ")
 
-  if guest_letter in computer_word:
+  player_guess = player_guess.lower() # Make guess lowercase for comparison
+
+  if player_guess in past_guesses:
+    print("\nYou already guessed '%s'. Try again." % player_guess)
+    continue
+  else:
+    past_guesses.append(player_guess)
+
+  if player_guess in computer_word:
     solved_word = True
 
     for letter in computer_word:
-      if letter not in past_guesses: solved_word =  False #Havn't guessed the full word yet
+      if letter not in past_guesses:
+        solved_word =  False # Haven't guessed the full word yet
 
     if solved_word:
-      print("\n", computer_word)
-      print("Congratulations, you guessed the word! \(^.^)/")
+      print("Congratulations! You guess the word:", computer_word, "! \(^.^)/")
       break
   else:
     guesses_left = guesses_left - 1
 
     if guesses_left == 0:
       print("Sorry, but you've run out of guesses. (~_~) ")
-      print("The word was %s." % computer_word)
+      print("The word was", computer_word)
       break
